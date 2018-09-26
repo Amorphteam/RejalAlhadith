cfef204a9af58fd5097d6189c556b5d9fa42c294
Mehdi Sohrabi <mehdok@gmail.com>
2018-09-26 20:36:00 +0330

    Add Menu items.

--- app/src/main/java/com/papyrus/mehdok/rejalalhadith/ui/viewer/TextViewer.kt
+++ app/src/main/java/com/papyrus/mehdok/rejalalhadith/ui/viewer/TextViewer.kt
@@ -2,6 +2,8 @@ package com.papyrus.mehdok.rejalalhadith.ui.viewer
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.Menu
+import android.view.MenuItem
 import com.papyrus.mehdok.rejalalhadith.R
 import kotlinx.android.synthetic.main.activity_text_viewer.*
 
@@ -11,7 +13,17 @@ class TextViewer : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_text_viewer)
         setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+    }
 
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        menuInflater.inflate(R.menu.menu_text_viewer, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return true
     }
 
 }
